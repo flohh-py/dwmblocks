4fd7043036562e86c53580abfbc3d3f3e6b65633
flohh-py <jbmflohh@gmail.com>
2022-05-06 18:54:21 -0400

    added scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,22 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    // {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",
+    // 30, 0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    // {"", "date '+%b %d (%a) %I:%M%p'", 5, 0},
+    // {"", "date '+%I:%M%p'", 60, 0},
+    {"", "sb-disk", 300, 0},
+    {"", "sb-loadavg", 1, 0},
+    {"", "sb-memory", 1, 0},
+    {"", "sb-volume", 0, 11},
+    {"", "sb-battery", 60, 0},
+    {"", "sb-network", 60, 0},
+    {"", "sb-date", 60, 0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
+static char delim[] = "";
 static unsigned int delimLen = 5;
